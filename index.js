9bf0a55035c5262108c3257ae847ba9a99e04113
Emilien Devos <contact@emiliendevos.be>
2020-01-29 13:40:50 +0100

    loop instead of a semi broken way to check if blocked

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import Proxy from "http-mitm-proxy";
 import * as antiCaptcha from "./anticaptcha.js";
 import { promises as fsPromises } from "fs";
 import fs from "fs";
+import got from "got";
+import delay from 'delay';
 
 const sslCAPath = (process.env.CA_PATH || process.cwd() + 'ca');
 const databaseFilePath = (process.env.DB_PATH || "./db.json");
@@ -19,42 +21,46 @@ if (fs.existsSync(databaseFilePath)) {
 
 const googleHosts = /(^www\.google\.[a-z]+$)|(^www\.youtube\.com$)|(^www\.youtu\.be$)/;
 
+async function checkIfBlocked() {
+    const response = await got.get("https://www.google.com/search?q=test", {
+        headers: {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
+            "accept-language": "en,en-US;q=0.9,fr;q=0.8,en-US;q=0.7",
+            "accept-encoding": "gzip, deflate, br",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+            "cookie": "GOOGLE_ABUSE_EXEMPTION=" + database.googleAbuseExemption
+        },
+        retry: 0,
+        throwHttpErrors: false
+    });
+    if (response.statusCode == 429) {
+        const json = await antiCaptcha.sendTask();
+        const taskId = json.taskId;
+        let status = "processing";
+        let gRecaptchaResponse;
+        while (status === "processing") {
+            const taskInfo = await antiCaptcha.getTask(taskId);
+            if (taskInfo.status === "ready") {
+                status = "ready";
+                gRecaptchaResponse = taskInfo.solution.gRecaptchaResponse;
+            }
+            await delay(10000);
+        }
+        database.googleAbuseExemption = await antiCaptcha.submitRecaptchaResponse(gRecaptchaResponse, response.url);
+        await fsPromises.writeFile(databaseFilePath, JSON.stringify(database), 'utf8');
+    }
+}
+
+setInterval(checkIfBlocked, 120000);
+
 proxy.onError(function (ctx, err) {
     console.error('proxy error:', err);
 });
 
 proxy.onRequest((ctx, callback) => {
     if (googleHosts.test(ctx.clientToProxyRequest.headers.host)) {
-        if (ctx.clientToProxyRequest.url.indexOf("/sorry") == 0) {
-            if (!database.taskId) {
-                database.status = "processing";
-                antiCaptcha.sendTask().then(json => {
-                    console.log("sending task");
-                    database.taskId = json.taskId;
-                }).catch(() => delete database.status);
-            }
-            else if (database.taskId && database.status === "processing") {
-                antiCaptcha.getTask(database.taskId).then(json => {
-                    if (json.status === "ready") {
-                        try {
-                            antiCaptcha.submitRecaptchaResponse(json.solution.gRecaptchaResponse, ctx.clientToProxyRequest.url).then(cookieGoogleAbuseExemption => {
-                                database.googleAbuseExemption = cookieGoogleAbuseExemption;
-                                delete database.taskId;
-                                delete database.status;
-                                fsPromises.writeFile(databaseFilePath, JSON.stringify(database), 'utf8');
-                            });
-                        } catch (error) {
-                            delete database.taskId;
-                            database.status = "error";
-                        }
-                    }
-                });
-            }
-        }
-        else {
-            if (database.googleAbuseExemption) {
-                ctx.proxyToServerRequestOptions.headers["Cookie"] = "GOOGLE_ABUSE_EXEMPTION=" + database.googleAbuseExemption;
-            }
+        if (database.googleAbuseExemption) {
+            ctx.proxyToServerRequestOptions.headers["Cookie"] = "GOOGLE_ABUSE_EXEMPTION=" + database.googleAbuseExemption;
         }
     }
     return callback();
